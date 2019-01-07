4e832b0f3a27b11b440d6bd256f7628ba26d443c
Radhika <radhikabhati413@gmail.com>
2019-01-07 01:38:15 -0700

    calculating odds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import * as d3 from 'd3';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 
 function Square(props) {
     return (
@@ -14,8 +16,19 @@ function Square(props) {
         super(props);
         this.state = {
           squares: Array(9).fill(null),
+          xpos: Array(5).fill(null),
+          opos:Array(5).fill(null),
           xIsNext: true,
+          xOdds:0.5,
+          oOdds:0.5,
+          oi:0,
+          xi:0,
+          Winner:0
         };
+      }
+      catchNumber(i){
+          console.log("catch number "+ i);
+          console.log(this.state.xpos);
       }
     renderSquare(i) {
         
@@ -25,6 +38,9 @@ function Square(props) {
      
     }
      handleClick(i) {
+        this.calculateOdds(i);
+   //this.catchNumber(i);
+   console.log(i);
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
         return;
@@ -35,13 +51,41 @@ function Square(props) {
       xIsNext: !this.state.xIsNext,
     });
   }
+ 
+  calculateOdds(i){
+      if(this.state.Winner!=1){
+        var  x =8;
+        var  o = 8;
+       
+          
+       console.log(this.state);
+       if(this.state.xIsNext){
+           //calculate o odds
+          this.state.opos[this.state.oi]=i;
+           this.state.oi++;
+        
   
+       }
+       else{
+           //calculate x odds
+           this.state.xpos[this.state.xi]= i;
+           this.state.xi++;
+       }
+        console.log(this.state.xpos);
+        console.log(this.state.opos);
+      }
+     
+  }
+
     render() {
+      
         const winner = calculateWinner(this.state.squares);
         let status;
         if (winner) {
+            this.state.Winner = 1;
           status = 'Winner: ' + winner;
         } else {
+           
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
   
@@ -75,6 +119,9 @@ function Square(props) {
           <div className="game-board">
             <Board />
           </div>
+          <div className="game-board">
+            <Visualization />
+          </div>
           <div className="game-info">
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
@@ -84,6 +131,20 @@ function Square(props) {
     }
   }
   
+  class Visualization extends React.Component{
+    render(){
+        return(
+            <div>
+                 <h1>hi im going to visualiztion the odds</h1>
+                 <svg style={{ width: this.props.width, height: this.props.height }}>
+               
+            </svg>
+            </div>
+           
+           
+        ); 
+      }
+  }
 
   function calculateWinner(squares) {
     const lines = [
@@ -110,4 +171,4 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
-  
+ 
